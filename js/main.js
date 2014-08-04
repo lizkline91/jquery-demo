524b0c07427fdfacb20d2c75f1e709455ec39083
lizkline91 <lizkline91@gmail.com>
2014-08-04 02:00:56 -0400

    jqueryhw
    partially completed :(

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// create on a single webpage
+// 3 photo albums that each contain 3 photos at least.
+// when you click a album, you will then only see the photos for that album, but you should be able to navigate back to view all of the albums
+// when clicking on individual photo in album, photo much enlarge and others in album will no longer be visible
+
+// use jquery to do this.
+$(document).ready(function() {
+
+  $("nav a").click(function(event) {
+
+    event.preventDefault();
+    var pageToNavigate = "." + $(this).attr("rel");
+    console.log(pageToNavigate);
+
+    $(".container").children().removeClass("currentPage");
+
+    $(pageToNavigate).addClass("currentPage");
+
+
+  });
+
+});
+
+
+//function jsTest() {
+
+//  var index = 0;
+//  var counter = 0;
+//  var obj = {};
+//  var func = function() {
+//    for (index = 0; index < 10; index++) {
+//      counter +=2;
+//    }
+//    obj.index++;
+//  };
+
+//  obj.func = func;
+//  this.index++;
+
+//  return index;
+//}
+
+//$('.gallery a').on('click', function(event) {
+//    event.preventDefault();
+    //$(this).closest('li').siblings().toggleClass('invisible');
+//    $(this).toggleClass('currentImage');
+//});
+$('.gallery img').hover(function(){
+    $(this).css({width:"500px", height:"500px"});
+},function(){
+    $(this).css({width:"100px", height:"100px"});
+});
+$('.gallery a').on('click', function(event) {
+  event.preventDefault();
+  $(this).closest('li').siblings().toggleClass('invisible');
+  $(this).toggleClass('currentImage');
+});
